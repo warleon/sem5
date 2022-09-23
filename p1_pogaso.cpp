f9780c8670bb775362c110c03faf459bb1012b64
Mauricio Bernuy <mauricio.bernuy@utec.edu.pe>
2022-09-23 16:20:09 -0500

    p1pog

--- p1_pogaso.cpp
+++ p1_pogaso.cpp
@@ -1,13 +1,16 @@
 #include <mpi.h>
+#include <math.h>
 #include <stdio.h>
 #include <iostream>
 using namespace std;
 
-const bool P = false; // toggle printear output
+const bool P = false; // toggle operation output
+const bool R = false; // toggle buffer result output
 
 int main(int argc, char **argv)
 {
-  int numtasks, rank, next, prev, tag1 = 1, tag2 = 2, N = 100000;
+  int numtasks, rank, next, prev, tag1 = 1, tag2 = 2;
+  size_t N = (size_t)pow(10,5);
   double sbuf[N], rbuf_prev[N], rbuf_next[N];
   double t0, t1;
   MPI_Status stats[4];
@@ -23,67 +26,75 @@ int main(int argc, char **argv)
     next = 0;
 
   srand(123456 + rank);
-  for (int i = 0; i < N; i++)
+  for (size_t i = 0; i < N; i++)
   {
     sbuf[i] = (double)rand()/RAND_MAX;
   }
-  cout <<"rank "<< rank << " SEND first/last: " << sbuf[0] <<", "<< sbuf[N-1] << endl;
-
+  if (R) cout <<"rank "<< rank << " SEND first/last: " << sbuf[0] <<", "<< sbuf[N-1] << endl;
 
   t0 = MPI_Wtime();
+
+  if (numtasks > 1){
   
-  //* SEND RECEIVE COUNTERCLOCKWISE
-  if (rank == 0)
-  {
-    if (P) cout << "sent to prev: " << prev << endl;
-    // send inicial, desbloquea el ultimo rank
-    // block hasta concluir receive en el ultimo rank
-    MPI_Send(sbuf, N, MPI_DOUBLE, prev, tag2, MPI_COMM_WORLD);
+    //* SEND RECEIVE COUNTERCLOCKWISE
+    if (rank == 0)
+    {
+      if (P) cout << "sent to prev: " << prev << endl;
+      // send inicial, desbloquea el ultimo rank
+      // block hasta concluir receive en el ultimo rank
+      MPI_Send(sbuf, N, MPI_DOUBLE, prev, tag2, MPI_COMM_WORLD);
+    }
+
+    if (P) cout << "receiving in rank: " << rank << endl;
+    // block hasta recibir send del rank next
+    MPI_Recv(rbuf_next, N, MPI_DOUBLE, next, tag2, MPI_COMM_WORLD, &stats[0]);
+    if (P) cout << "received rank: " << rank << endl;
+
+    if (rank != 0)
+    {
+      if (P) cout << "sent to prev: " << prev << endl;
+      // desbloquea rank prev, block hasta concluir receive de prev
+      MPI_Send(sbuf, N, MPI_DOUBLE, prev, tag2, MPI_COMM_WORLD);
+    }
+
+  /*----------------------------------------------------*/
+
+    //* SEND RECEIVE CLOCKWISE
+    if (rank == 0)
+    {
+      if (P) cout << "sent to next: " << next << endl;
+      // send inicial, desbloquea el rank 1
+      // block hasta concluir receive en el rank 1
+      MPI_Send(sbuf, N, MPI_DOUBLE, next, tag1, MPI_COMM_WORLD);
+    }
+
+    if (P) cout << "receiving in rank: " << rank << endl;
+    // block hasta recibir send del rank prev
+    MPI_Recv(rbuf_prev, N, MPI_DOUBLE, prev, tag1, MPI_COMM_WORLD, &stats[0]);
+    if (P) cout << "received rank: " << rank << endl;
+
+    if (rank != 0)
+    {
+      if (P) cout << "sent to next: " << next << endl;
+      // desbloquea rank next, block hasta concluir receive de next
+      MPI_Send(sbuf, N, MPI_DOUBLE, next, tag1, MPI_COMM_WORLD);
+    }
+
   }
-
-  if (P) cout << "receiving in rank: " << rank << endl;
-  // block hasta recibir send del rank next
-  MPI_Recv(rbuf_next, N, MPI_DOUBLE, next, tag2, MPI_COMM_WORLD, &stats[0]);
-  if (P) cout << "received rank: " << rank << endl;
-
-  if (rank != 0)
-  {
-    if (P) cout << "sent to prev: " << prev << endl;
-    // desbloquea rank prev, block hasta concluir receive de prev
-    MPI_Send(sbuf, N, MPI_DOUBLE, prev, tag2, MPI_COMM_WORLD);
-  }
-
-/*----------------------------------------------------*/
-
-  //* SEND RECEIVE CLOCKWISE
-  if (rank == 0)
-  {
-    if (P) cout << "sent to next: " << next << endl;
-    // send inicial, desbloquea el rank 1
-    // block hasta concluir receive en el rank 1
-    MPI_Send(sbuf, N, MPI_DOUBLE, next, tag1, MPI_COMM_WORLD);
-  }
-
-  if (P) cout << "receiving in rank: " << rank << endl;
-  // block hasta recibir send del rank prev
-  MPI_Recv(rbuf_prev, N, MPI_DOUBLE, prev, tag1, MPI_COMM_WORLD, &stats[0]);
-  if (P) cout << "received rank: " << rank << endl;
-
-  if (rank != 0)
-  {
-    if (P) cout << "sent to next: " << next << endl;
-    // desbloquea rank next, block hasta concluir receive de next
-    MPI_Send(sbuf, N, MPI_DOUBLE, next, tag1, MPI_COMM_WORLD);
+  else{
+    memcpy(rbuf_prev, sbuf,N*sizeof*sbuf);
+    memcpy(rbuf_next, sbuf,N*sizeof*sbuf);
   }
 
   if (P) cout << "finished rank: " << rank << endl;
   t1 = MPI_Wtime();
 
   if (rank == 0)
-    cout << "Tiempo (s): " << t1 - t0 << endl;
+    cout << "Processors: " << numtasks <<", N: "<< N 
+    <<", Time (ms): " << 1000*(t1 - t0) << endl;
 
-  cout <<"rank "<< rank << " PREV first/last: " << rbuf_prev[0] <<", "<< rbuf_prev[N-1] << endl;
-  cout <<"rank "<< rank << " NEXT first/last: " << rbuf_next[0] <<", "<< rbuf_next[N-1] << endl;
+  if (R) cout <<"rank "<< rank << " PREV first/last: " << rbuf_prev[0] <<", "<< rbuf_prev[N-1] << endl;
+  if (R) cout <<"rank "<< rank << " NEXT first/last: " << rbuf_next[0] <<", "<< rbuf_next[N-1] << endl;
 
 
   MPI_Finalize();
