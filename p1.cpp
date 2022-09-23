56e0d0e75fdcca3eb70669a1e5018df006f7b14a
warleon <51892390+warleon@users.noreply.github.com>
2022-09-23 17:07:49 -0500

    xd

--- p1.cpp
+++ p1.cpp
@@ -1,4 +1,5 @@
 
+#include <math.h>
 #include <mpi.h>
 #include <stdio.h>
 
@@ -8,73 +9,79 @@
 
 using namespace std;
 
-std::atomic<int> msgRecv, msgSend;
-std::vector<bool> sflag1, sflag2;
+const bool P = true;  // toggle operation output
+const bool R = true;  // toggle buffer result output
 
-int main(int argc, char **argv)
-{
-  std::cout << "GAAAAAAAAAAA" << std::endl;
+int main(int argc, char **argv) {
   int numtasks, rank, next, prev;
   //   int buf[2];
-  int tag1 = 1, tag2 = 2, N = 1024;
-  int sbuf[N], rbuf[N];
+  int tag1 = 1, tag2 = 2;
+  // size_t N = (size_t)pow(10, 1);
+  size_t N = (size_t)2;
+
+  double sbuf[N], rbuf_prev[N], rbuf_next[N];
+  //  N = 1024;
+  // int sbuf[N], rbuf[N];
+  MPI_Status stats[4];
+
   double t0, t1;
   int pendingMessage;
+  int msgRecv, msgSend;
+  bool sendNext = false;
+  bool sendBefore = false;
   //   MPI_Status stats[4];
-  MPI_Init(&argc, &argv);                   // Initialize MPI
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);     // Rank of the processor
-  MPI_Comm_size(MPI_COMM_WORLD, &numtasks); // Total number of processors
+  MPI_Init(&argc, &argv);                    // Initialize MPI
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);      // Rank of the processor
+  MPI_Comm_size(MPI_COMM_WORLD, &numtasks);  // Total number of processors
 
   //   MPI_Request reqs[4];
 
   prev = (rank - 1 + numtasks) % numtasks;
   next = (rank + 1 + numtasks) % numtasks;
 
-  if (int(sflag1.size()) != numtasks)
-  {
-    sflag1.resize(numtasks);
+  srand(123456 + rank);
+  for (size_t i = 0; i < N; i++) {
+    sbuf[i] = (double)rand() / RAND_MAX;
   }
-  if (int(sflag2.size()) != numtasks)
-  {
-    sflag2.resize(numtasks);
-  }
+  if (R)
+    cout << "rank " << rank << " SEND first/last: " << sbuf[0] << ", "
+         << sbuf[N - 1] << endl;
+
   t0 = MPI_Wtime();
-  do
-  {
-    MPI_Iprobe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &pendingMessage,
-               MPI_STATUS_IGNORE);
-    if (pendingMessage)
-    {
-      std::cout << "pendingMessage if finished" << std::endl;
-      MPI_Recv(rbuf, N, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD,
-               MPI_STATUS_IGNORE);
-      msgRecv++;
-
-    }
-    if (msgSend != msgRecv && !sflag1[rank])
-    {
-      std::cout << "sflag1 if finished" << std::endl;
-      msgSend++;
-      sflag1[rank] = 1;
-      MPI_Send(sbuf, N, MPI_INT, next, tag1, MPI_COMM_WORLD);
-
-    }
-    if (msgSend != msgRecv && !sflag2[rank])
-    {
-      std::cout << "sflag2 if finished" << std::endl;
-      msgSend++;
-      sflag2[rank] = 1;
-      MPI_Send(sbuf, N, MPI_INT, prev, tag2, MPI_COMM_WORLD);
-    }
-    std::cout << "a: " << msgRecv << std::endl;
-
-  } while (msgSend == 2 * numtasks && msgRecv == 2 * numtasks);
+  if (rank % 2) {
+    MPI_Recv(rbuf_prev, N, MPI_INT, prev, MPI_ANY_TAG, MPI_COMM_WORLD,
+             MPI_STATUS_IGNORE);
+    MPI_Recv(rbuf_next, N, MPI_INT, next, MPI_ANY_TAG, MPI_COMM_WORLD,
+             MPI_STATUS_IGNORE);
+  } else {
+    MPI_Send(sbuf, N, MPI_INT, next, tag1, MPI_COMM_WORLD);
+    MPI_Send(sbuf, N, MPI_INT, prev, tag2, MPI_COMM_WORLD);
+  }
+  if (!(rank % 2)) {
+    MPI_Recv(rbuf_prev, N, MPI_INT, prev, MPI_ANY_TAG, MPI_COMM_WORLD,
+             MPI_STATUS_IGNORE);
+    MPI_Recv(rbuf_next, N, MPI_INT, next, MPI_ANY_TAG, MPI_COMM_WORLD,
+             MPI_STATUS_IGNORE);
+  } else {
+    MPI_Send(sbuf, N, MPI_INT, next, tag1, MPI_COMM_WORLD);
+    MPI_Send(sbuf, N, MPI_INT, prev, tag2, MPI_COMM_WORLD);
+  }
+  if (P) cout << "finished rank: " << rank << endl;
 
   t1 = MPI_Wtime();
 
+  // if (rank == 0) cout << "Tiempo (s): " << t1 - t0 << endl;
+
   if (rank == 0)
-    cout << "Tiempo (s): " << t1 - t0 << endl;
-  std::cout << "smurfeados" << std::endl;
+    cout << "Processors: " << numtasks << ", N: " << N
+         << ", Time (ms): " << 1000 * (t1 - t0) << endl;
+
+  if (R)
+    cout << "rank " << rank << " PREV first/last: " << rbuf_prev[0] << ", "
+         << rbuf_prev[N - 1] << endl;
+  if (R)
+    cout << "rank " << rank << " NEXT first/last: " << rbuf_next[0] << ", "
+         << rbuf_next[N - 1] << endl;
 
   MPI_Finalize();
 }
--- p1_pogaso.cpp
+++ p1_pogaso.cpp
@@ -1,44 +1,42 @@
-#include <mpi.h>
 #include <math.h>
+#include <mpi.h>
 #include <stdio.h>
+
 #include <iostream>
+
 using namespace std;
 
-const bool P = false; // toggle operation output
-const bool R = false; // toggle buffer result output
+const bool P = true;  // toggle operation output
+const bool R = true;  // toggle buffer result output
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   int numtasks, rank, next, prev, tag1 = 1, tag2 = 2;
-  size_t N = (size_t)pow(10,5);
+  size_t N = (size_t)pow(10, 5);
   double sbuf[N], rbuf_prev[N], rbuf_next[N];
   double t0, t1;
   MPI_Status stats[4];
-  MPI_Init(&argc, &argv);                   // Initialize MPI
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);     // Rank of the processor
-  MPI_Comm_size(MPI_COMM_WORLD, &numtasks); // Total number of processors
+  MPI_Init(&argc, &argv);                    // Initialize MPI
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);      // Rank of the processor
+  MPI_Comm_size(MPI_COMM_WORLD, &numtasks);  // Total number of processors
 
   prev = rank - 1;
   next = rank + 1;
-  if (rank == 0)
-    prev = numtasks - 1;
-  if (rank == (numtasks - 1))
-    next = 0;
+  if (rank == 0) prev = numtasks - 1;
+  if (rank == (numtasks - 1)) next = 0;
 
   srand(123456 + rank);
-  for (size_t i = 0; i < N; i++)
-  {
-    sbuf[i] = (double)rand()/RAND_MAX;
+  for (size_t i = 0; i < N; i++) {
+    sbuf[i] = (double)rand() / RAND_MAX;
   }
-  if (R) cout <<"rank "<< rank << " SEND first/last: " << sbuf[0] <<", "<< sbuf[N-1] << endl;
+  if (R)
+    cout << "rank " << rank << " SEND first/last: " << sbuf[0] << ", "
+         << sbuf[N - 1] << endl;
 
   t0 = MPI_Wtime();
 
-  if (numtasks > 1){
-  
+  if (numtasks > 1) {
     //* SEND RECEIVE COUNTERCLOCKWISE
-    if (rank == 0)
-    {
+    if (rank == 0) {
       if (P) cout << "sent to prev: " << prev << endl;
       // send inicial, desbloquea el ultimo rank
       // block hasta concluir receive en el ultimo rank
@@ -50,18 +48,16 @@ int main(int argc, char **argv)
     MPI_Recv(rbuf_next, N, MPI_DOUBLE, next, tag2, MPI_COMM_WORLD, &stats[0]);
     if (P) cout << "received rank: " << rank << endl;
 
-    if (rank != 0)
-    {
+    if (rank != 0) {
       if (P) cout << "sent to prev: " << prev << endl;
       // desbloquea rank prev, block hasta concluir receive de prev
       MPI_Send(sbuf, N, MPI_DOUBLE, prev, tag2, MPI_COMM_WORLD);
     }
 
-  /*----------------------------------------------------*/
+    /*----------------------------------------------------*/
 
     //* SEND RECEIVE CLOCKWISE
-    if (rank == 0)
-    {
+    if (rank == 0) {
       if (P) cout << "sent to next: " << next << endl;
       // send inicial, desbloquea el rank 1
       // block hasta concluir receive en el rank 1
@@ -73,29 +69,30 @@ int main(int argc, char **argv)
     MPI_Recv(rbuf_prev, N, MPI_DOUBLE, prev, tag1, MPI_COMM_WORLD, &stats[0]);
     if (P) cout << "received rank: " << rank << endl;
 
-    if (rank != 0)
-    {
+    if (rank != 0) {
       if (P) cout << "sent to next: " << next << endl;
       // desbloquea rank next, block hasta concluir receive de next
       MPI_Send(sbuf, N, MPI_DOUBLE, next, tag1, MPI_COMM_WORLD);
     }
 
-  }
-  else{
-    memcpy(rbuf_prev, sbuf,N*sizeof*sbuf);
-    memcpy(rbuf_next, sbuf,N*sizeof*sbuf);
+  } else {
+    memcpy(rbuf_prev, sbuf, N * sizeof *sbuf);
+    memcpy(rbuf_next, sbuf, N * sizeof *sbuf);
   }
 
   if (P) cout << "finished rank: " << rank << endl;
   t1 = MPI_Wtime();
 
   if (rank == 0)
-    cout << "Processors: " << numtasks <<", N: "<< N 
-    <<", Time (ms): " << 1000*(t1 - t0) << endl;
+    cout << "Processors: " << numtasks << ", N: " << N
+         << ", Time (ms): " << 1000 * (t1 - t0) << endl;
 
-  if (R) cout <<"rank "<< rank << " PREV first/last: " << rbuf_prev[0] <<", "<< rbuf_prev[N-1] << endl;
-  if (R) cout <<"rank "<< rank << " NEXT first/last: " << rbuf_next[0] <<", "<< rbuf_next[N-1] << endl;
-
+  if (R)
+    cout << "rank " << rank << " PREV first/last: " << rbuf_prev[0] << ", "
+         << rbuf_prev[N - 1] << endl;
+  if (R)
+    cout << "rank " << rank << " NEXT first/last: " << rbuf_next[0] << ", "
+         << rbuf_next[N - 1] << endl;
 
   MPI_Finalize();
 }
