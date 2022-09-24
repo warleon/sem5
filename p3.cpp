334f07a772dc348b1f594ae0c33da85f4a7c527f
warleon <51892390+warleon@users.noreply.github.com>
2022-09-23 21:38:09 -0500

    Update p3.cpp

--- p3.cpp
+++ p3.cpp
@@ -1,10 +1,11 @@
+#include <math.h>
 #include <mpi.h>
 #include <stdio.h>
 int main(void) {
   int my_rank, comm_sz;
 
   const size_t N{1 << 5};
-  int mat[N][N]{}, vec[N]{}, x[N]{};
+  double mat[N][N]{}, vec[N]{}, x[N]{};
 
   MPI_Init(NULL, NULL);
   MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
@@ -12,15 +13,21 @@ int main(void) {
 
   // rellenar el vector y matriz
   if (!my_rank) {
+    for (size_t i = 0; i < N; i++) {
+      vec[i] = (double)rand() / RAND_MAX;
+      for (size_t j = 0; j < N; j++) mat[i][j] = (double)rand() / RAND_MAX;
+      std::cout << vec[i] << " ";
+    }
+    std::cout << "\n";
   }
 
   // comunicacion
   size_t my_rowsBegin = (N / comm_sz) * my_rank;
   size_t my_workload = N / comm_sz;
   size_t my_rowsEnd = my_rowsBegin + N / my_workload;
-  MPI_Bcast(vec, N, MPI_INT, 0, MPI_COMM_WORLD);
-  MPI_Scatter(mat, N * N, MPI_INT, &mat[my_rowsBegin][0], my_workload * N,
-              MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Bcast(vec, N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+  MPI_Scatter(mat, N * N, MPI_DOUBLE, &mat[my_rowsBegin][0], my_workload * N,
+              MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
   // Procesar
   for (size_t i = 0; i < my_workload; i++) {
@@ -28,8 +35,14 @@ int main(void) {
   }
 
   // comunicacion
-  MPI_Gather(x, my_workload, MPI_INT, vec, my_workload, MPI_INT, 0,
+  MPI_Gather(x, my_workload, MPI_DOUBLE, vec, my_workload, MPI_DOUBLE, 0,
              MPI_COMM_WORLD);
+  if (!my_rank) {
+    for (size_t i = 0; i < N; i++) {
+      std::cout << vec[i] << " ";
+    }
+    std::cout << "\n";
+  }
 
   MPI_Finalize();
   return 0;
